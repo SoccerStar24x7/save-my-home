0383cb76d854cddd41d2294906af63d1d965ef6b
SoccerStar24x7 <superzap77@gmail.com>
2025-05-07 17:18:54 -0400

    yep

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ import pandas as pd
 
 
 #Set seed for reproducibilty
+
 def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
@@ -44,7 +45,7 @@ set_seed(42)
 
 @dataclass(frozen=True)
 class TrainingConfig:
-      ''' Configuration for Training '''
+      # Configuration for Training
       batch_size: int = 32
       num_epochs: int = 100
       learning_rate: float = 1e-4
@@ -58,3 +59,70 @@ class TrainingConfig:
 train_config = TrainingConfig()
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 print("Available Device: ", DEVICE)
+
+
+train_root = os.path.join("testing_data", "training")
+val_root = os.path.join(train_config.data_root, "testing_data", "validation")
+
+img_size = (224,224)
+
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(img_size, antialias=True),
+        transforms.ToTensor()
+    ]
+)
+
+# determines the mean based on the given dataset, which in our case are the photos of the stove
+def get_mean_std(train_loader, img_size=(224, 224), num_workers=2):
+
+    batch_mean = torch.zeros(3)
+    batch_mean_sqrd = torch.zeros(3)
+
+    for batch_data, _ in train_loader:
+        batch_mean += batch_data.mean(dim=(0, 2, 3))  # E[batch_i]
+        batch_mean_sqrd += (batch_data**2).mean(dim=(0, 2, 3))  #  E[batch_i**2]
+
+    mean = batch_mean / len(train_loader) # calculates the mean
+
+    var = (batch_mean_sqrd / len(train_loader)) - (mean**2)
+
+    std = var**0.5
+
+    return mean, std # returns the mean and the std of the given dataset
+
+
+train_data_mean_calc = datasets.ImageFolder(root=train_root, transform = preprocess) # gets the training info
+train_loader_mean_calc = DataLoader(train_data_mean_calc, shuffle = True, batch_size = train_config.batch_size, num_workers = train_config.num_workers)
+
+mean, std = get_mean_std(train_loader_mean_calc) # gets the mean and std of the dataset
+
+# makes sure to normalize any data given
+common_transforms = transforms.Compose(
+    [
+        preprocess,
+        transforms.Normalize(mean=mean,std=std)
+    ]
+)
+
+# performs augmentations on the training data to produce more training data out of the given training data
+train_transforms = transforms.Compose(
+    [
+        preprocess,
+        transforms.RandomHorizontalFlip(),
+
+        transforms.RandomApply([
+        transforms.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75)),
+         ], p =0.1),
+
+        transforms.Normalize(mean = mean,std = std)
+    ]
+)
+
+
+#Apply the augmentations to the training dataset
+train_data = datasets.ImageFolder(root = train_root, transform = train_transforms)
+
+# The validation dataset should have only common transforms like Resize, ToTensor and Normalize.
+val_data = datasets.ImageFolder(root=val_root, transform = common_transforms)
+
