ddaff9f2d4f45bf2ceae0f3679d6ad55cdad4d97
SoccerStar24x7 <superzap77@gmail.com>
2025-05-07 18:56:39 -0400

    made changes

--- main.py
+++ main.py
@@ -302,8 +302,6 @@ def main(model, train_loader, val_loader):
         val_losses.append(val_loss)
         val_accuracies.append(val_accuracy)
 
-        print(f"Epoch {epoch+1:0>2}/{train_config.num_epochs} - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}% - Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%")
-
         # Logging metrics to tensorboard
         writer.add_scalar('Loss/train', train_loss)
         writer.add_scalar('Loss/val', val_loss)
@@ -313,15 +311,72 @@ def main(model, train_loader, val_loader):
         if val_accuracy > best_val_acc:
             best_val_acc = val_accuracy
             best_weights =  model.state_dict()
-            print(f"Saving best model...💾")
+
             torch.save(best_weights, "best.pt")
 
     return train_losses, train_accuracies, val_losses, val_accuracies
 
 
-# ------------------------------------------- Save the best model---------------------------------
+# ------------------------------------------- Save the best model ---------------------------------
 
 # Load the best model weights
 model.load_state_dict(torch.load("best.pt"))
 model.eval()
 
+
+# ---------------------------------- The mode can now make inferences ------------------------
+def prediction(model, val_loader):
+
+    model.eval()
+    model.to(DEVICE)
+
+    all_images, all_labels = [], []
+    all_pred_indices, all_pred_probs = [], []
+
+    for images, labels in val_loader:
+        images, labels = images.to(DEVICE), labels.to(DEVICE)
+
+        with torch.inference_mode():
+             outputs = model(images)
+
+        prob = F.softmax(outputs,dim=1)
+        pred_indices = prob.data.max(dim=1)[1]
+        pred_probs = prob.data.max(dim=1)[0]
+
+        all_images.append(images.cpu())
+        all_labels.append(labels.cpu())
+        all_pred_indices.append(pred_indices.cpu())
+        all_pred_probs.append(pred_probs.cpu())
+
+
+    return (torch.cat(all_images).numpy(),
+            torch.cat(all_labels).numpy(),
+            torch.cat(all_pred_indices).numpy(),
+            torch.cat(all_pred_probs).numpy())
+
+
+def denormalize(image):
+    mean_ar = np.array(mean)
+    std_ar = np.array(std)
+    image = image * std_ar + mean_ar
+    return np.clip(image, 0,1)
+
+def visualise_predictions(sample_images,sample_gt_labels, pred_indices, pred_probs, num_images =5):
+
+    fig = plt.figure(figsize = (20,5))
+
+    for i in range(num_images):
+        idx = random.randint(0, len(sample_images) -1)
+        image = sample_images[idx].transpose(1,2,0) #(C,H,W) --> (H,W,C)
+        label = sample_gt_labels[idx]
+        pred_idx = pred_indices[idx]
+        pred_prob = pred_probs[idx]
+
+        image = denormalize(image)
+
+        ax = fig.add_subplot(1, num_images, i+1)
+        ax.imshow(image)
+        ax.set_title(f"GT: {class_mapping[label]}\nPred: {class_mapping[pred_idx]} ({pred_prob:.2f})")
+        ax.axis('off')
+
+    plt.show()
